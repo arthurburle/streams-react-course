120ca3d8a24da9320b700d3b0213409951aeb88c
Arthur Burle <arthurburle@gmail.com>
2020-10-29 13:42:25 -0300

    streams first commit

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,17 @@
+import { SIGN_IN, SIGN_OUT } from '../actions/types';
+
+const INITIAL_STATE = {
+    isSignedIn: null,
+    userId: null
+}; // Tudo maiúsculo é sinal de que o valor não deve ser mudado
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case SIGN_IN:
+            return { ...state, isSignedIn: true, userId: action.payload }
+        case SIGN_OUT:
+            return { ...state, isSignedIn: false, userId: null }
+        default:
+            return state;
+    }
+};
