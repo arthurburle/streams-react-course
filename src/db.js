772850e941f923fdb1462f56582017dc66eb29e2
Arthur Burle <arthurburle@gmail.com>
2020-10-30 01:40:20 -0300

    another

---
+++ src/db.js
@@ -0,0 +1,32 @@
+export const streamsFakeAPI = [
+  {
+    title: "My stream",
+    description: "This is a great stream",
+    id: 1,
+  },
+  {
+    title: "My second stream",
+    description: "This also a great stream",
+    id: 2,
+  },
+  {
+    title: "I created this stream",
+    description: "This stream is another one",
+    id: 3,
+  },
+  {
+    title: "My stream",
+    description: "This is a great stream",
+    id: 4,
+  },
+  {
+    title: "My second stream",
+    description: "This also a great stream",
+    id: 5,
+  },
+  {
+    title: "I created this stream",
+    description: "This stream is another one",
+    id: 6,
+  },
+];
